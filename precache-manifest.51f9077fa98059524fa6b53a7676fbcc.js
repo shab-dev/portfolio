995195f98899e5b84cd7b1bc0feb96f48f62e166
Waqas nazir <vickydev31113@gmail.com>
2021-01-10 13:25:28 +0500

    Updates

---
+++ precache-manifest.51f9077fa98059524fa6b53a7676fbcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c45fd6f57843e464711552b06dff67",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e24e9802e8d732a42a9",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e24e9802e8d732a42a9",
+    "url": "/portfolio/static/js/main.062e9dc8.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
